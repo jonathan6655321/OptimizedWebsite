5825237d9c7cac74848f7bf786b82130f0a886d0
Jonathan Somer <somer@mail.tau.ac.il>
2016-09-05 21:17:04 +0300

    two loops for updatePositions()

--- views/js/main.js
+++ views/js/main.js
@@ -502,9 +502,14 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
+  var newLeftCoordinates = [];
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+    var currentPhase = Math.sin((document.body.scrollTop / 1250) + (i % 5))
+    newLeftCoordinates.push(items[i].basicLeft + 100 * currentPhase + 'px');
+  }
+
+  for (var i = 0; i < items.length; i++) {
+    items[i].style.left = newLeftCoordinates[i];
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
