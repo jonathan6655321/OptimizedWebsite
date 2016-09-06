d287f40fefca6b5173dd5df1a4a46f0d4e25d2a6
Jonathan Somer <somer@mail.tau.ac.il>
2016-09-06 18:50:33 +0300

    a

--- views/js/main.js
+++ views/js/main.js
@@ -503,7 +503,7 @@ function updatePositions() {
   var scrollPosition = $(window).scrollTop();
   var currentPhaseList = [];
   for (i = 0; i < 5; i++) {
-    currentPhaseList.push(Math.sin(scrollPosition / 1250));
+    currentPhaseList.push(Math.sin((scrollPosition + i*5) / 1250) );
   }
 
   for (var i = 0; i < itemsLength; i += 1) {
